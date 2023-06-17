b046d7af1752487893a4cc2af72268d82905a4d2
KKR02 <karthi03.r@gmail.com>
2023-06-17 14:04:56 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 
 
 import React from 'react'
+import { Route, Routes } from "react-router-dom"
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Notfound from './pages/Notfound'
+import Header from './units/Header'
+
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+
+
+
+    </div>
   )
 }
 
